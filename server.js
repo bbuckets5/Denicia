7be6c4e1e00878b087a5bc6dd685c1b67c287add
Brianna <briannathagoat@outlook.com>
2025-08-02 12:49:36 -0400

    refactor: Restructure files into public folder for Vercel

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(express.static(path.join(__dirname, '/')));
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
