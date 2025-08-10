43848169bb13824cd0f1123cc8b909666e557d4b
Brianna <briannathagoat@outlook.com>
2025-08-09 22:59:39 -0400

    Restructure project with public folder for Vercel

--- server.js
+++ server.js
@@ -42,11 +42,10 @@ app.use(
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+
+app.use(express.static('public'));
+
 
 // --- RATE LIMITING MIDDLEWARE ---
 const apiLimiter = rateLimit({
