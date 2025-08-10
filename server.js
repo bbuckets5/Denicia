c0fdcb35e978536e01109709b7b6ebb6315df00c
Brianna <briannathagoat@outlook.com>
2025-08-10 12:15:25 -0400

    still vercel debuffing

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // --- RATE LIMITING MIDDLEWARE ---
