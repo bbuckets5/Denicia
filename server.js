7b7cec2d4a7b062b6ca3a152c39f02ebd12d775e
Brianna <briannathagoat@outlook.com>
2025-08-09 23:22:47 -0400

    vercel debugging again

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const { log } = require("console");
 
 const app = express();
+app.use((req, res, next) => {
+  console.log(`[Vercel Log] Incoming Request: ${req.method} ${req.url}`);
+  next();
+});
 const PORT = process.env.PORT || 3000;
 
 // --- CLOUDINARY CONFIGURATION ---
